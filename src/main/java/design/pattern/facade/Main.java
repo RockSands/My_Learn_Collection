b9baf491ebd78f37f3d2b6da4d104d6232bbc853
RockSands <117019049@qq.com>
2017-05-16 10:43:23 +0800

    门面模式

---
+++ src/main/java/design/pattern/facade/Main.java
@@ -0,0 +1,11 @@
+package design.pattern.facade;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ModuleFacade facade = new ModuleFacade();
+		facade.a1();
+		facade.b1();
+		facade.c1();
+	}
+}
---
+++ src/main/java/design/pattern/facade/ModuleA.java
@@ -0,0 +1,19 @@
+package design.pattern.facade;
+
+public class ModuleA {
+	/**
+	 * 提供给子系统外部使用的方法
+	 */
+	public void a1() {
+		System.out.println("ModuleA.a1()");
+	};
+
+	/**
+	 * 子系统内部模块之间相互调用时使用的方法
+	 */
+	public void a2() {
+	};
+
+	public void a3() {
+	};
+}
