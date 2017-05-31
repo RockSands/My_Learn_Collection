9e7002115261148f94f38aa3782c24089e56f1d0
RockSands <117019049@qq.com>
2017-05-31 14:20:22 +0800

    submit

--- src/main/java/sql/jsqlparser/create/DeMain.java
+++ src/main/java/sql/jsqlparser/create/DeMain.java
@@ -1,7 +1,6 @@
 package sql.jsqlparser.create;
 
 import java.io.StringReader;
-import java.util.List;
 
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserManager;
@@ -14,26 +13,32 @@ public class DeMain {
 	public static void main(String[] args) throws JSQLParserException {
 		String create = "CREATE TABLE `MyTable1` (" + "`col1`  varchar(255) NOT NULL COMMENT '注释1' ,"
 				+ "`col2`  varchar(255) NOT NULL COMMENT '注释2' ,"
-				+ "`col3`  varchar(255) NOT NULL DEFAULT 0 COMMENT '注释3' " + ")" + "COMMENT='我的表'" + ";";
+				+ "`col3`  varchar(255) NOT NULL DEFAULT 0 COMMENT '注释3',PRIMARY KEY (`col1`,`col2`) " + ")" + "COMMENT='我的表'"
+				+ ";";
 		CCJSqlParserManager parserManager = new CCJSqlParserManager();
 		Statement statement = parserManager.parse(new StringReader(create));
 		CreateTable createTable = (CreateTable) statement;
 		for (ColumnDefinition index : createTable.getColumnDefinitions()) {
 			System.out.println("=ColumnDefinition=>" + index);
 			System.out.println("=ColumnDefinition.ColumnName=>" + index.getColumnName());
-			System.out.println("=ColumnDefinition.ColDataType=>" + index.getColDataType());
-			System.out.println("=ColumnDefinition.ColDataType.DataType=>" + index.getColDataType().getDataType());
-			System.out.println("=ColumnDefinition.ColDataType.ArgumentsStringList=>" + index.getColDataType().getArgumentsStringList());
-			System.out.println("=ColumnDefinition.ColDataType.ArrayData=>" + index.getColDataType().getArrayData());
-			//System.out.println("=ColumnDefinition.ColumnSpecStrings=>" + index.getColumnSpecStrings());
+			// System.out.println("=ColumnDefinition.ColDataType=>" +
+			// index.getColDataType());
+			// System.out.println("=ColumnDefinition.ColDataType.DataType=>" +
+			// index.getColDataType().getDataType());
+			// System.out.println("=ColumnDefinition.ColDataType.ArgumentsStringList=>"
+			// + index.getColDataType().getArgumentsStringList());
+			// System.out.println("=ColumnDefinition.ColDataType.ArrayData=>" +
+			// index.getColDataType().getArrayData());
+			// System.out.println("=ColumnDefinition.ColumnSpecStrings=>" +
+			// index.getColumnSpecStrings());
 		}
 		System.out.println();
-		if(createTable.getCreateOptionsStrings() != null){
+		if (createTable.getCreateOptionsStrings() != null) {
 			for (String index : createTable.getCreateOptionsStrings()) {
 				System.out.println("=CreateOptionsString=>" + index);
 			}
 		}
-		if(createTable.getTableOptionsStrings() != null){
+		if (createTable.getTableOptionsStrings() != null) {
 			for (Object index : createTable.getTableOptionsStrings()) {
 				System.out.println("=TableOptionsStrings=>" + index);
 			}
--- src/main/java/sql/jsqlparser/create/Main.java
+++ src/main/java/sql/jsqlparser/create/Main.java
@@ -12,23 +12,24 @@ import net.sf.jsqlparser.statement.create.table.CreateTable;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		// 服务
 		CreateTable createTable = new CreateTable();
 		// 表
 		createTable.setIfNotExists(true);
 		createTable.setTable(new Table());
-		createTable.getTable().setName("Table1");
-		createTable.setTableOptionsStrings(Arrays.asList("COMMENT", "=", "'我的表'"));
+		createTable.getTable().setName("Table1");//变量
+		createTable.setTableOptionsStrings(Arrays.asList("COMMENT", "=", "'我的表'"));//表注释变量
 		// 列
 		List<ColumnDefinition> columnDefinitions = new ArrayList<ColumnDefinition>();
 		for (int i = 0; i < 3; i++) {
 			ColumnDefinition columnDefinition = new ColumnDefinition();
-			columnDefinition.setColumnName("COL" + i);
+			columnDefinition.setColumnName("COL" + i);//字段名
 			ColDataType colDataType = new ColDataType();
-			colDataType.setDataType("varchar");
-			colDataType.setArgumentsStringList(Arrays.asList("255"));
+			colDataType.setDataType("varchar");//类型 + 长度
+			colDataType.setArgumentsStringList(Arrays.asList("255"));//
 			columnDefinition.setColDataType(colDataType);
-			columnDefinition.setColumnSpecStrings(Arrays.asList("NOT", "NULL", "DEFAULT 0", "COMMENT '注释" + i + "'"));
+			columnDefinition.setColumnSpecStrings(Arrays.asList("NOT", "NULL", "DEFAULT 0", "COMMENT '注释" + i + "'"));// NOT NULL Default,注释
+			//某些列是
 			columnDefinitions.add(columnDefinition);
 		}
 		createTable.setColumnDefinitions(columnDefinitions);
